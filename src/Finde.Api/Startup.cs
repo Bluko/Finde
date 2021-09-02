79984d58c59238e0a5c4a646559c2d194448e64c
Mikołaj Kowalski <mikolaj.p.kowalski@gmail.com>
2021-09-02 19:28:52 +0200

    up

---
+++ src/Finde.Api/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Autofac;
+using System;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Finde.Core.Repositories;
+using Finde.Infrastructure.EF;
+using Finde.Infrastructure.IoC;
+using Finde.Infrastructure.IoC.Modules;
+using Finde.Infrastructure.Mapper;
+using Finde.Infrastructure.Repositories;
+using Finde.Infrastructure.Services;
+using System.ComponentModel;
+using IContainer = Autofac.IContainer;
+
+namespace Finde.Api
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            builder.RegisterModule<CommandModule>();
+            builder.RegisterModule(new ContainerModule(Configuration));
+            ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(ApplicationContainer);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            IApplicationLifetime applicationLifetime)
+        {
+
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+            app.UseMvc();
+            applicationLifetime.ApplicationStopped.Register(() => ApplicationContainer.Dispose());       
+        }
+    }
+}
